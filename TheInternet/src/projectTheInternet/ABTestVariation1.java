7579434464acc8cfc3eb92bc7a284ca57ce81a65
Deepti02 <saaikec@gmail.com>
2017-06-03 16:28:23 -0400

    Initial Commit - Deepti

---
+++ TheInternet/src/projectTheInternet/ABTestVariation1.java
@@ -0,0 +1,28 @@
+package projectTheInternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ABTestVariation1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver;		
+		System.setProperty("wedriver.chrome.driver", "C:\\Users\\saaik\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		String baseUrl ="http://the-internet.herokuapp.com";
+		driver.get(baseUrl);
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//a[@href='/abtest']")).click();
+		System.out.println("Clicked");
+		
+		
+
+	}
+
+}
